f08a69844f3c5f6f4ecf82de85e59b69809d1aeb
r-hall <ryanhall2015@gmail.com>
2018-03-07 11:54:18 -0800

    all tests passed for queue functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  var counter = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    size++;
+    counter++;
+    storage[counter] = value;
   };
 
   someInstance.dequeue = function() {
+    if (size > 0) {
+      size--;
+      result = storage[counter-size];
+      return result;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
