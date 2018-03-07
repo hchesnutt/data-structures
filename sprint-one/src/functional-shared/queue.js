a4f78697e6a6668f36ad56db9f57875b1c12a73a
r-hall <ryanhall2015@gmail.com>
2018-03-07 12:24:28 -0800

    all tests passed

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {
+  	'storage': {},
+  	'numElements': 0,
+  	'counter': 0
+  };
+  for (var key in queueMethods) {
+  	someInstance[key] = queueMethods[key];
+  }
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+	enqueue: function(value) {
+    	this.numElements++;
+    	this.counter++;
+    	this.storage[this.counter] = value;
+  	},
+  	dequeue: function() {
+    	if (this.numElements > 0) {
+      		this.numElements--;
+      		result = this.storage[this.counter - this.numElements];
+      		return result;
+    	}
+  	},
+  	size: function() {
+    	return this.numElements;
+  	}
+};
+
 
 
