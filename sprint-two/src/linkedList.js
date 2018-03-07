1634350cb9c8de51eeb763c5341820f3d639345b
hchesnutt <t.henry.chesnutt@gmail.com>
2018-03-07 15:34:47 -0800

    pass all linkedList tests

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    // create new node
+    var newNode = Node(value);
+    // if tail exists, point curr tail to newnode and point tail to newnode
+    if (list.tail) {
+      list.tail.next= newNode;
+      list.tail = newNode;
+    } else {
+    // else set tail and head to newnode
+      list.head = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
+    // save old head
+    var temp = list.head;
+    // set head to head's next
+    list.head = list.head.next;
+    // return old head
+    return temp.value;
   };
 
   list.contains = function(target) {
+    var current = list.head;
+    while (current) {
+      if (current.value === target) {
+        return true
+      }
+      current = current.next;
+    }
+    return false;
   };
 
   return list;
