84733480d145bd20d3120d8093e20a34ed585379
r-hall <ryanhall2015@gmail.com>
2018-03-07 12:55:25 -0800

    all tests passed

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.storage = {},
+  	this.numElements = 0,
+  	this.counter = 0
+  }
+
+  enqueue(value) {
+    this.numElements++;
+    this.counter++;
+    this.storage[this.counter] = value;
+  }
+  	
+  dequeue() {
+    if (this.numElements > 0) {
+      	this.numElements--;
+      	var result = this.storage[this.counter - this.numElements];
+      	return result;
+    }
+  }
+  	
+  size() {
+    return this.numElements;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,30 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+  	this.storage = {};
+  	this.counter = 0;
+  }
+
+  push(value) {
+	this.counter++;
+    this.storage[this.counter] = value;
+  }
+
+  pop() {
+    if (this.counter > 0) {
+      	for (var key in this.storage) {
+        	if (key == this.counter) {
+          		var result = this.storage[key];
+          		break;
+        	}
+      	}
+      	this.counter--;
+      	return result;
+    }
+  }
+  	
+  size() {
+  	return this.counter;
   }
 
 }
