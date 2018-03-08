7aaae37d565969d3d52383b63f240f84ac75ae0c
hchesnutt <t.henry.chesnutt@gmail.com>
2018-03-07 21:46:55 -0800

    pass all tests for hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,33 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  if (Array.isArray(this._storage.get(index))) {
+    this._storage.get(index).push([k,v]);
+  } else {
+    this._storage.set(index, [[k,v]]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var result;
+  this._storage.get(index).forEach(tuple => {
+    if (tuple[0] === k) {
+      result = tuple[1];
+    }
+  });
+  return result;
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var parent = this._storage.get(index);
+  for (var i = 0; i < parent.length; i++) {
+    if (parent[i][0] === k) {
+      parent.splice(i, 1);
+      break;
+    }
+  }
 };
 
 
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item) {
+  return !!this._storage[item];
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ add: constant
+ contains: constant
+ remove: constant
+ 
+ for later use: http://www.pdiniz.com/time-complexity-of-javascripts-built-in-methods/
  */
