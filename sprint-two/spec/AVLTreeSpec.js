f81a393ee9d3639eeca76d1e25ff984e197a812a
hchesnutt <t.henry.chesnutt@gmail.com>
2018-03-09 15:59:20 -0800

    pass insert test for AVL tree

--- sprint-two/spec/AVLTreeSpec.js
+++ sprint-two/spec/AVLTreeSpec.js
@@ -1,28 +1,38 @@
-Descibe('AVL Tree', function() {
-  BeforeEach(function() {
-    let avlTree = new AVLTree(20);
+describe('AVL Tree', function() {
+  var avlTree;
+  
+  beforeEach(function() {
+    avlTree = new AVLTree();
   });
 
   it('should have insert and contains methods', function() {
     expect(avlTree.insert).to.be.a('function');
+    expect(avlTree.delete).to.be.a('function');
     expect(avlTree.contains).to.be.a('function');
   });
+  
   it('should insert in binary tree style', function() {
-    let values = [5, 7, 8, 6];
+    var values = [5, 7, 8, 6];
     values.forEach(value => avlTree.insert(value));
     expect(avlTree._root.right.value).to.equal(7);
     expect(avlTree._root.right.right.value).to.equal(8);
     expect(avlTree._root.right.left.value).to.equal(6);
   });
 
-  it('should have a working contains method', function() {
-    let values = [5, 7, 8, 6];
+  it('should have a working delete method', function() {
+    var values = [5, 7, 8, 6];
     values.forEach(value => avlTree.insert(value));
     values.forEach(value => expect(avlTree.contains(value)).to.be.true);
   });
+
+  it('should have a working contains method', function() {
+    var values = [5, 7, 8, 6];
+    values.forEach(value => avlTree.insert(value));
+    values.forEach(value => expect(avlTree.contains(value)).to.be.true);
+  });    
   
   it('should implement a correct outer right rotation', function() {
-    let values = [5, 6, 7];
+    var values = [5, 6, 7];
     values.forEach(value => avlTree.insert(value));
     expect(avlTree._root.value).to.equal(6);
     expect(avlTree._root.left.value).to.equal(5);
@@ -30,7 +40,7 @@ Descibe('AVL Tree', function() {
   });
   
   it('should implement a correct outer left rotation', function() {
-    let values = [7, 6, 5];
+    var values = [7, 6, 5];
     values.forEach(value => avlTree.insert(value));
     expect(avlTree._root.value).to.equal(7);
     expect(avlTree._root.left.value).to.equal(5);
---
+++ sprint-two/src/AVLTree.js
@@ -0,0 +1,91 @@
+class AVLTree {
+  constructor() {
+    this._root = null;
+  }
+  
+  insert(value) {
+    var newNode = this.AVLNode(value);
+    // BST insert with height update
+    this.bstInsert(newNode, this._root);
+    console.log(this._root)
+    // check for imbalanced nodes, calls cb on node
+    this.checkForImbalance(newNode);
+    // if left or right imbalance
+    // check appropriate child imbalance for outer or inner
+    // pass imbalanced node to balancing function
+  }
+  
+  delete(value) {
+    // return deleted value
+  }
+  
+  contains(value) {
+    // return t/f
+  }
+  
+  checkForImbalance(currNode) {
+    var imbalance = (this.getHeight(currNode.left) + 1) - (this.getHeight(currNode.right) + 1);
+    if (imbalance < -1) {
+      this.rebalance(currNode, 'right');
+      checkForImbalance(currNode.parent);
+    }
+    if (imbalance > 1) {
+      this.rebalance(currNode, 'left');
+      checkForImbalance(currNode.parent);
+    }
+    if(!currNode.parent) {
+      return;
+    }
+  }
+  
+  getHeight(node) {
+    if (!node) {
+      return -1;
+    }
+    if (!node.right && !node.left) {
+      return 0;
+    } else if(!node.right) {
+      return node.left.height;
+    } else if(!node.left) {
+      return node.right.height;
+    } else {
+      return Math.max(node.right.height, node.left.height) + 1;
+    }
+  }
+  
+  bstInsert(node, currNode) {
+    if (!this._root) {
+      this._root = node;
+      return this._root;
+    } else {
+      node.height++;
+      if (node.value > currNode.value) {
+        if (!currNode.right) {
+          currNode.right = node; // if node should go right and there's no right child, insert here
+          node.parent = currNode.right;
+        } else {
+          this.bstInsert(node, currNode.right);
+        }
+      }
+      if (node.value < currNode.value) {
+        if (!currNode.left) {
+          currNode.left = node;
+          node.parent = currNode.left;
+        } else {
+          this.bstInsert(node, currNode.left);
+        }
+      }
+    }
+    return node;
+  }
+  
+  AVLNode(value) {
+    var newNode = {};
+    newNode.value = value;
+    newNode.height = 0;
+    newNode.parent = null;
+    newNode.left = null;
+    newNode.right = null;
+    return newNode;
+  } 
+}
