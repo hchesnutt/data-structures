3fe1a70c5409b5c0adb6b14845ecc507f2772430
hchesnutt <t.henry.chesnutt@gmail.com>
2018-03-08 20:20:27 -0800

    pass dynamic resizing tests for hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,6 +1,7 @@
 
 
 var HashTable = function() {
+  this._count = 0;
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
 };
@@ -12,11 +13,16 @@ HashTable.prototype.insert = function(k, v) {
   } else {
     this._storage.set(index, [[k,v]]);
   }
+  this._count++;
+  this.checkResize();
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var result;
+  if (!this._storage.get(index)) {
+    return undefined;
+  }
   this._storage.get(index).forEach(tuple => {
     if (tuple[0] === k) {
       result = tuple[1];
@@ -34,9 +40,37 @@ HashTable.prototype.remove = function(k) {
       break;
     }
   }
+  this._count--;
+  this.checkResize();
 };
 
+HashTable.prototype.checkResize = function() {
+  if (this._count / this._limit > 0.75) {
+    this.resize(this._limit * 2);
+  }
+  if (this._count / this._limit < 0.25) {
+    this.resize(Math.floor(this._limit / 2));
+  }
+};
 
+HashTable.prototype.resize = function(newSize) {
+  var tempArray = LimitedArray(newSize);
+  this._storage.each(array => {
+    if(array) {
+      for (var i = 0; i < array.length; i++) {
+        var index = getIndexBelowMaxForKey(array[i][0], newSize);
+        if (Array.isArray(tempArray.get(index))) {
+          tempArray.get(index).push([array[i][0],array[i][1]]);
+        } else {
+          tempArray.set(index, [[array[i][0],array[i][1]]]);
+        }
+      }
+    }
+  });
+  // set storage to tempArray
+  this._storage = tempArray;
+  this._limit = newSize;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
